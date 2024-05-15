dbc7e83325bafc4c94598f1e3b4e642c4b9ec1f3
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-05-15 16:11:44 +0100

    variables created

--- hangman.py
+++ hangman.py
@@ -1,10 +1,18 @@
 import random
+import string
 from words import words
 
 def generateWord(words):
     word = random.choice(words)
     if '-' in word:
         word = random.choice(words)
-    return word
+    return word.upper()
     
-generateWord(words)
+def hangman():
+    word = generateWord(words)
+    correctLetters = set(word) 
+    letters = set(string.ascii_uppercase)
+    selectedLetters = set()
+    playerLetter = input('Choose a letter => ').upper()
+    
+hangman()
