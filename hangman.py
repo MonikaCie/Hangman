b6d8db59a9310a283021e44d651279369397ba95
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-05-15 17:04:38 +0100

    Update hangman.py
    Lives added

--- hangman.py
+++ hangman.py
@@ -7,17 +7,21 @@ def generateWord(words):
     if '-' in word:
         word = random.choice(words)
     return word.upper()
-    
+
+
+
 def hangman():
     word = generateWord(words)
     correctLetters = set(word) #Letters present in the word
     letters = set(string.ascii_uppercase) #All letters
     selectedLetters = set() #Letters selected by a player
     
-    while len(correctLetters) > 0:
+    lives = 10
+    
+    while len(correctLetters) > 0 and lives > 0:
         
         #Used letters
-        print('\nUsed letters: ' , ' '.join(selectedLetters), '\n')
+        print('\nUsed letters: ' , ' '.join(selectedLetters))
         
         #Currently guessed letters in the word
         wordLetters = []
@@ -28,18 +32,25 @@ def hangman():
                 wordLetters.append('*')
         print(' '.join(wordLetters))
                 
-        
-                
     
-        playerLetter = input('Choose a letter => ').upper()
+        playerLetter = input(f'Lives left: {lives}\nChoose a letter => ').upper()
         
         if playerLetter in letters and playerLetter not in selectedLetters:
             selectedLetters.add(playerLetter)
             if playerLetter in correctLetters:
                 correctLetters.remove(playerLetter)
+            else:
+                lives = lives - 1
+                print(f'{playerLetter} not in the word.')
+                
         elif playerLetter in selectedLetters:
             print(f'Letter {playerLetter} already used. Try a different letter.')
         else:
             print(f'Letter {playerLetter} is invalid. Try a different letter.')
-        
+    
+    if lives == 0:
+        print(f'GAME OVER!\nThe word was: {word}')
+    else:
+        print(f'Congratulations! You guessed {word} correctly')
+    
 hangman()
