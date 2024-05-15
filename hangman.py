23e71a6cb66262ffc04bf94f17e92dcf2c15f921
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-05-15 16:46:43 +0100

    Update hangman.py
    used letters listed
    correctly guessed letters

--- hangman.py
+++ hangman.py
@@ -13,11 +13,33 @@ def hangman():
     correctLetters = set(word) #Letters present in the word
     letters = set(string.ascii_uppercase) #All letters
     selectedLetters = set() #Letters selected by a player
-    playerLetter = input('Choose a letter => ').upper()
     
-    if playerLetter in letters and playerLetter not in selectedLetters:
-        selectedLetters.add(playerLetter)
-        if playerLetter in correctLetters:
-            correctLetters.remove(playerLetter)
+    while len(correctLetters) > 0:
+        
+        #Used letters
+        print('\nUsed letters: ' , ' '.join(selectedLetters), '\n')
+        
+        #Currently guessed letters in the word
+        wordLetters = []
+        for letter in word:
+            if letter in selectedLetters:
+                wordLetters.append(letter)
+            else:
+                wordLetters.append('*')
+        print(' '.join(wordLetters))
+                
+        
+                
     
+        playerLetter = input('Choose a letter => ').upper()
+        
+        if playerLetter in letters and playerLetter not in selectedLetters:
+            selectedLetters.add(playerLetter)
+            if playerLetter in correctLetters:
+                correctLetters.remove(playerLetter)
+        elif playerLetter in selectedLetters:
+            print(f'Letter {playerLetter} already used. Try a different letter.')
+        else:
+            print(f'Letter {playerLetter} is invalid. Try a different letter.')
+        
 hangman()
